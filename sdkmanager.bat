6a1be643a9e0103cf2b6e8e211f67d2e989092b7
Benoi Abraham <benoi.abraham@ba.com>
2026-06-30 13:28:04 +0530

    Hardcode corporate proxy settings directly in local sdkmanager.bat wrapper script

--- sdkmanager.bat
+++ sdkmanager.bat
@@ -1,32 +1,3 @@
 @echo off
-setlocal enabledelayedexpansion
-
-set PROXY_HOST=
-set PROXY_PORT=
-
-set INPUT_FILE=%~dp0input.txt
-
-if not exist "%INPUT_FILE%" (
-    echo Error: input.txt not found at %INPUT_FILE%
-    exit /b 1
-)
-
-for /f "usebackq tokens=1,2 delims==" %%A in ("%INPUT_FILE%") do (
-    set "key=%%A"
-    set "value=%%B"
-    
-    rem Strip spaces from key and value
-    set "key=!key: =!"
-    set "value=!value: =!"
-    
-    if "!key!"=="systemProp.http.proxyHost" set "PROXY_HOST=!value!"
-    if "!key!"=="systemProp.http.proxyPort" set "PROXY_PORT=!value!"
-)
-
-if not "%PROXY_HOST%"=="" (
-    echo [Proxy Helper] Found proxy settings in input.txt: %PROXY_HOST%:%PROXY_PORT%
-    call C:\Users\n533750.WW\Documents\Software\cmdline-tools\latest\bin\sdkmanager.bat --proxy=http --proxy_host=%PROXY_HOST% --proxy_port=%PROXY_PORT% %*
-) else (
-    echo [Proxy Helper] No proxy settings found in input.txt, running directly.
-    call C:\Users\n533750.WW\Documents\Software\cmdline-tools\latest\bin\sdkmanager.bat %*
-)
+echo [Proxy Helper] Running sdkmanager with hardcoded corporate proxy: baproxy-cloud.baia.baplc.com:8082
+call C:\Users\n533750.WW\Documents\Software\Android\cmdline-tools\latest\bin\sdkmanager.bat --proxy=http --proxy_host=baproxy-cloud.baia.baplc.com --proxy_port=8082 %*
