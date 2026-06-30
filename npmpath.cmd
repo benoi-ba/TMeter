d37ba1fe6edac2cb2b40a620cdbf2513657f943a
Benoi Abraham <benoi.abraham@ba.com>
2026-06-30 13:35:50 +0530

    Fixed error in the @activity_main.xml

--- npmpath.cmd
+++ npmpath.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-set PATH=%PATH%;C:\Users\n533750.WW\Documents\Software\node-v26.1.0-win-x64;C:\Users\n533750.WW\Documents\Software\cmdline-tools\latest\bin;C:\Users\n533750.WW\Documents\Software\cmdline-tools\platform-tools
+set PATH=%PATH%;C:\Users\n533750.WW\Documents\Software\node-v26.1.0-win-x64;C:\Users\n533750.WW\Documents\Software\Android\cmdline-tools\latest\bin;C:\Users\n533750.WW\Documents\Software\Android\platform-tools
 
