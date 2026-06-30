51326b9250b6ed2a45ff8eee74194f7622c5169c
Benoi Abraham <benoi.abraham@ba.com>
2026-06-30 12:35:09 +0530

    Add project-local sdkmanager proxy wrapper reading configuration from input.txt, and revert session-level Java proxy edits

--- npmpath.cmd
+++ npmpath.cmd
@@ -1,3 +1,4 @@
 @echo off
 
-set PATH=%PATH%;C:\Users\n533750.WW\Documents\Software\node-v26.1.0-win-x64
+set PATH=%PATH%;C:\Users\n533750.WW\Documents\Software\node-v26.1.0-win-x64;C:\Users\n533750.WW\Documents\Software\cmdline-tools\latest\bin;C:\Users\n533750.WW\Documents\Software\cmdline-tools\platform-tools
+
